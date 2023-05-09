dc757296c6dd637bf8764f547788b05e840415cb
Alejandro <Alecayasa15@gmail.com>
2023-05-09 20:31:09 +0800

    Initialial Author Route

--- server.js
+++ server.js
@@ -6,15 +6,29 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+
+
+
 const { parse } = require('dotenv')
+
+
+
+
+
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 
 
@@ -25,9 +39,18 @@ mongoose.connect(
 
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
-db.once('open', () => console.log('Connected to MongoDB Atlas'));
+db.once('open', () => console.log('Connection Successful' + '\n' + 'Connected to MongoDB' + '\n' + 'Welcome Alejandro'));
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+'authors/new'
 
 app.listen(process.env.PORT || 3000)
+
+
+
+// Commands Shown:
+// Get-ExecutionPolicy -List
+// Set-ExecutionPolicy Unrestricted
+// Set-ExecutionPolicy Unrestricted -Force
